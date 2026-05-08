2b35b51221ba0530cfb85b0f01451baa36ccb38c
lavanyaamehrotra <144808904+lavanyaamehrotra@users.noreply.github.com>
2026-05-09 02:50:51 +0530

    [Lavanya Mehrotra] Connect frontend to production gateway

--- ConnectHub_Frontend/src/environments/environment.prod.ts
+++ ConnectHub_Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080/api',
-  hubUrl: 'http://localhost:8080/hubs',
+  apiUrl: 'https://gateway-service-hjaw.onrender.com/api',
+  hubUrl: 'https://gateway-service-hjaw.onrender.com/hubs',
   googleClientId: '288824054265-bbvt754hpo80e64gm9o5vj3o9o0umkl7.apps.googleusercontent.com'
 };
